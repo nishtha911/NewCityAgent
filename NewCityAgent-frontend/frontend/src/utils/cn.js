bdba5e56317b8990f2dfa89e1c548eef7cf460e2
Nishtha <nishthapardesi2005@gmail.com>
2026-06-28 12:19:12 +0530

    separating into two folders for ease

--- NewCityAgent-frontend/frontend/src/utils/cn.js
+++
@@ -1,9 +0,0 @@
-export function cn(...args) {
-  return args
-    .flat(Infinity)
-    .filter(Boolean)
-    .filter((x) => typeof x === 'string')
-    .join(' ')
-    .replace(/\s+/g, ' ')
-    .trim()
-}
